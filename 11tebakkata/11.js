9a53a5b6e4decbe6c6cf14f9e4c1bd2ebb75b64a
Riko <rifamuqtafa@gmail.com>
2021-10-13 21:46:21 +0700

    added challange 11

---
+++ 11tebakkata/11.js
@@ -0,0 +1,33 @@
+const fs = require('fs')
+const { stdin } = require('process')
+let data = fs.readFileSync('data.json', 'utf-8')
+let ambil = data.split(/[""]/)
+let soal = [ambil[3], ambil[11], ambil[19]]
+let jwbn = [ambil[7], ambil[15], ambil[23]]
+console.log("Selamat datang di permainan Tebak Kata, silahkan isi dengan jawaban yang benar ya! \n")
+
+const readline = require('readline')
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+rl.setPrompt("Tebakan: ")
+console.log(`Pertanyaan: ${soal[0]}`)
+rl.prompt()
+let i = 0;
+rl.on('line', (jawab) => {
+    if (jawab == `${jwbn[i]}`) {
+        console.log("Selamat Anda Benar! \n");
+        i++
+        if (i == soal.length){
+            console.log("Hore Anda Menang!");
+            rl.close();
+        }
+        console.log(`Pertanyaan: ${soal[i]}`)
+    } else {
+        console.log("Wkwkwkwk, Anda kurang beruntung!\n")
+    }
+    rl.prompt()
+}).on('close',()=>{
+    process.exit(0);
+})
