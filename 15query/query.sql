4c8a7e79b5dc7ee0713017635f609dce488a958f
Riko <rifamuqtafa@gmail.com>
2021-10-19 08:15:51 +0700

    added challange 15

--- 15query/query.sql
+++ 15query/query.sql
@@ -17,14 +17,14 @@ WHEN strftime('%m', date('now')) = strftime('%m', date(ttl)) THEN
 WHEN strftime('%m', date('now')) < strftime('%m', date(ttl)) THEN strftime('%Y', date('now')) - strftime('%Y', date(ttl))-1
 END AS 'umur'
 FROM mahasiswa
-WHERE umur < 20
+WHERE umur < 20;
 
 --, no 3
 SELECT krs.nim, mahasiswa.namamahasiswa, matakuliah.namamatkul, krs.nilai
 FROM krs
 INNER JOIN mahasiswa ON krs.nim = mahasiswa.nim 
 INNER JOIN matakuliah ON krs.kodematkul = matakuliah.kodematkul
-WHERE nilai = 'A' OR nilai = 'B'
+WHERE nilai = 'A' OR nilai = 'B';
 
 --, no 4
 SELECT krs.nim, mahasiswa.namamahasiswa, SUM(matakuliah.sks) AS totalsks
@@ -32,21 +32,21 @@ FROM krs
 INNER JOIN mahasiswa ON krs.nim = mahasiswa.nim
 INNER JOIN matakuliah ON krs.kodematkul = matakuliah.kodematkul
 GROUP BY krs.nim, mahasiswa.namamahasiswa
-HAVING totalsks > 10
+HAVING totalsks > 10;
 
 --, no 5
 SELECT krs.nim, mahasiswa.namamahasiswa, matakuliah.namamatkul
 FROM krs
 INNER JOIN mahasiswa ON krs.nim = mahasiswa.nim 
 INNER JOIN matakuliah ON krs.kodematkul = matakuliah.kodematkul
-WHERE matakuliah.namamatkul = 'Data Mining'
+WHERE matakuliah.namamatkul = 'Data Mining';
 
 --, no 6
 SELECT matakuliah.nip, dosen.namadosen, COUNT(DISTINCT krs.nim) AS totalmahasiswa
 FROM matakuliah
 INNER JOIN dosen ON matakuliah.nip = dosen.nip
 INNER JOIN krs ON matakuliah.kodematkul = krs.kodematkul
-GROUP BY matakuliah.nip, dosen.namadosen
+GROUP BY matakuliah.nip, dosen.namadosen;
 
 --, no 7
 SELECT nim, namamahasiswa,
@@ -60,7 +60,7 @@ WHEN strftime('%m', date('now')) = strftime('%m', date(ttl)) THEN
 WHEN strftime('%m', date('now')) < strftime('%m', date(ttl)) THEN strftime('%Y', date('now')) - strftime('%Y', date(ttl))-1
 END AS 'umur'
 FROM mahasiswa
-ORDER BY umur ASC
+ORDER BY umur ASC;
 
 --, no 8 using JOIN
 SELECT krs.nim, mahasiswa.namamahasiswa, mahasiswa.alamat, mahasiswa.ttl, jurusan.namajurusan, jurusan.kodejurusan, matakuliah.namamatkul, matakuliah.kodematkul, krs.nilai, dosen.namadosen, dosen.nip
@@ -69,7 +69,7 @@ INNER JOIN mahasiswa ON krs.nim = mahasiswa.nim
 INNER JOIN matakuliah ON krs.kodematkul = matakuliah.kodematkul
 INNER JOIN jurusan ON matakuliah.kodejurusan = jurusan.kodejurusan 
 INNER JOIN dosen ON matakuliah.nip = dosen.nip
-WHERE krs.nilai = 'D' OR krs.nilai = 'E'
+WHERE krs.nilai = 'D' OR krs.nilai = 'E';
 
 --, no 8 using WHERE
 SELECT krs.nim, mahasiswa.namamahasiswa, mahasiswa.alamat, mahasiswa.ttl, jurusan.namajurusan, jurusan.kodejurusan, matakuliah.namamatkul, matakuliah.kodematkul, krs.nilai, dosen.namadosen, dosen.nip
@@ -79,4 +79,4 @@ WHERE krs.nim = mahasiswa.nim
     AND matakuliah.kodejurusan = jurusan.kodejurusan 
     AND matakuliah.nip = dosen.nip 
     AND (krs.nilai = 'D' 
-    OR krs.nilai = 'E') 
+    OR krs.nilai = 'E'); 
