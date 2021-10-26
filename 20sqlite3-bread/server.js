afc9b6ba46a538ab4f15ffad5740517cbf884a01
Riko <rifamuqtafa@gmail.com>
2021-10-26 21:57:50 +0700

    added challange 20

--- 20sqlite3-bread/server.js
+++ 20sqlite3-bread/server.js
@@ -37,46 +37,76 @@ app.use(bodyParser.json())
 app.use('/', express.static(path.join(__dirname, 'public')))
 
 app.get('/', function (req, res) {
-
+    let data = dataraw;
     filter = req.query
-    if (filter.IDcheck == 'IDcheck' || filter.stringcheck == 'stringcheck' || filter.integercheck == 'integercheck' || filter.floatcheck == 'floatcheck' || filter.datecheck == 'datecheck' || filter.booleancheck == 'booleancheck') {
-        if (filter.IDcheck == 'IDcheck') {
+    let filtered = []
+    if (filter.IDcheck == 'IDcheck') {
+        if (filter.IDfilter) {
             let i = Number(filter.IDfilter)
-            let a = data[i - 1]
-            data = []
-            data.push(a)
-        }
-        data = data.filter(filtering)
-        function filtering(f) {
-            if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.booleandb == filter.booleanfilter }
-            else if (filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.datecheck == 'datecheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter && f.stringdb == filter.stringfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck') { return f.stringdb == filter.stringfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.stringcheck == 'stringcheck' && filter.floatcheck == 'floatcheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.floatdb == filter.floatfilter && f.stringdb == filter.stringfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.stringdb == filter.stringfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck') { return f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.integercheck == 'integercheck' && filter.floatcheck == 'floatcheck' && filter.booleancheck == 'booleancheck') { return f.integerdb == filter.integerfilter && f.floatdb == filter.floatfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.integercheck == 'integercheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.integerdb == filter.integerfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.booleandb == filter.booleanfilter }
-            else if (filter.floatcheck == 'floatcheck' && filter.datecheck == 'datecheck' && filter.booleancheck == 'booleancheck') { return f.floatdb == filter.floatfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.integercheck == 'integercheck') { return f.stringdb == filter.stringfilter && f.integerdb == filter.integerfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.floatcheck == 'floatcheck') { return f.stringdb == filter.stringfilter && f.floatdb == filter.floatfilter }
-            else if (filter.stringcheck == 'stringcheck' && filter.datecheck == 'datecheck') { return f.stringdb == filter.stringfilter && new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.stringcheck == 'stringcheck' && filter.booleancheck == 'booleancheck') { return f.stringdb == filter.stringfilter && f.booleandb == filter.booleanfilter }
-            else if (filter.stringcheck == 'stringcheck') { return f.stringdb == filter.stringfilter }
-            else if (filter.integercheck == 'integercheck') { return f.integerdb == filter.integerfilter }
-            else if (filter.floatcheck == 'floatcheck') { return f.floatdb == filter.floatfilter }
-            else if (filter.datecheck == 'datecheck') { return new Date(f.datedb) >= new Date(filter.datefilter) && new Date(f.datedb) <= new Date(filter.datefilter2) }
-            else if (filter.booleancheck == 'booleancheck') { return f.booleandb == filter.booleanfilter }
-            else { return f }
+            filtered.push(data[i - 1])
+        } else { filtered = data }
+    } else { filtered = data }
+    if (filter.stringcheck == 'stringcheck') {
+        if (filter.stringfilter) {
+            let tempfilter = []
+            for (let i = 0; i < filtered.length; i++) {
+                if (filtered[i].stringdb.includes(filter.stringfilter)) { tempfilter.push(filtered[i]) }
+            }
+            filtered = tempfilter
         }
     }
-    else { data = dataraw }
-
+    if (filter.integercheck == 'integercheck') {
+        if (filter.integerfilter) {
+            let tempfilter = []
+            for (let i = 0; i < filtered.length; i++) {
+                if (filtered[i].integerdb == filter.integerfilter) { tempfilter.push(filtered[i]) }
+            }
+            filtered = tempfilter
+        }
+    }
+    if (filter.floatcheck == 'floatcheck') {
+        if (filter.floatfilter) {
+            let tempfilter = []
+            for (let i = 0; i < filtered.length; i++) {
+                if (filtered[i].floatdb == filter.floatfilter) { tempfilter.push(filtered[i]) }
+            }
+            filtered = tempfilter
+        }
+    }
+    if (filter.datecheck == 'datecheck') {
+        if (filter.datefilter) {
+            if (filter.datefilter2) {
+                let tempfilter = []
+                for (let i = 0; i < filtered.length; i++) {
+                    if (new Date(filtered[i].datedb) >= new Date(filter.datefilter) && new Date(filtered[i].datedb) <= new Date(filter.datefilter2)) { tempfilter.push(filtered[i]) }
+                }
+                filtered = tempfilter
+            } else {
+                let tempfilter = []
+                for (let i = 0; i < filtered.length; i++) {
+                    if (new Date(filtered[i].datedb) >= new Date(filter.datefilter)) { tempfilter.push(filtered[i]) }
+                }
+                filtered = tempfilter
+            }
+        } else if (filter.datefilter2) {
+            let tempfilter = []
+            for (let i = 0; i < filtered.length; i++) {
+                if (new Date(filtered[i].datedb) <= new Date(filter.datefilter2)) { tempfilter.push(filtered[i]) }
+            }
+            filtered = tempfilter
+        }
+    }
+    if (filter.booleancheck == 'booleancheck') {
+        if (filter.booleanfilter) {
+            let tempfilter = []
+            for (let i = 0; i < filtered.length; i++) {
+                if (filtered[i].booleandb == filter.booleanfilter) { tempfilter.push(filtered[i]) }
+            }
+            filtered = tempfilter
+        }
+    }
+    data = filtered;
+    console.log(data)
     let url = req._parsedUrl.query
     let urlstring = `${url}`
     let urls = urlstring.split("=")
@@ -92,7 +122,6 @@ app.get('/', function (req, res) {
     let pageno = req.query.page ? Number(req.query.page) : 1
     let offset = 3 * (pageno - 1)
     let data2 = data.slice(offset, offset + 3)
-
     res.render('index', { data: data2, totalpage, currentpage: pageno, offset, filter, url })
 })
 app.get('/add', function (req, res) {
